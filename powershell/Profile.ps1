73b5ec6818f209482a209700dbbd0fbca4116f25
Robin Ricard <git@rricard.me>
2022-12-07 13:34:39 +0000

    powershell: load aliases

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -5,3 +5,16 @@ if ($poshGitModule) {
 } else {
 	echo "Install posh-git: https://github.com/dahlbyk/posh-git#installation"
 }
+
+# 2. Load alias config data
+$aliasFiles = Get-ChildItem $env:XDG_CONFIG_HOME/aliases/*.csv
+foreach ($aliasFile in $aliasFiles) {
+  $aliases = Import-Csv $aliasFile -Header "name","command"
+  foreach ($alias in $aliases) {
+    $fn_name = "AliasFunction$($alias.name)"
+    $invoke = "function $fn_name(`$0) { $($alias.command) `$0 }"
+    echo "$invoke"
+    Invoke-Expression "$invoke"
+    Set-Alias -Name $alias.name -Value $fn_name
+  }
+}
