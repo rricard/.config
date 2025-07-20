cd6a1ecad006dcdad460a00f0dc26642f500422a
Robin Ricard <git@rricard.me>
2025-07-20 18:50:28 +0100

    No plugin lsp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,7 @@
 -- loads up the "universal" config loadable in classic vim
 vim.cmd('source ~/.config/nvim/vimrc')
 
--- initialize plugins via lazy.nvim
-require('plugins')
-
--- nvim-dependent configs
-vim.cmd.colorscheme "catppuccin-latte"
+-- LSPs
+vim.lsp.enable('luals')
+vim.lsp.enable('json')
 
