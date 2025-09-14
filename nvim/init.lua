43c350b1efc512b59f412d177fd104baa5b2b6b5
Robin Ricard <git@rricard.me>
2025-09-14 11:51:29 +0100

    autoformat

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,3 +5,26 @@ vim.cmd('source ~/.config/nvim/vimrc')
 vim.lsp.enable('luals')
 vim.lsp.enable('ocamllsp')
 
+-- Autoformat
+vim.api.nvim_create_augroup('AutoFormatting', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.lua',
+  group = 'AutoFormatting',
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
+
+local opam_bin = vim.fn.trim(vim.fn.system('opam var bin'))
+local ocamllformat_bin = opam_bin .. "/ocamlformat"
+vim.api.nvim_create_autocmd(
+  "BufWritePost",
+  {
+    pattern = "*.ml,*.mli,dune",
+    group = "AutoFormatting",
+    callback = function()
+      vim.cmd(" silent !" .. ocamllformat_bin .. " -i %")
+      vim.cmd("edit")
+    end,
+  }
+)
